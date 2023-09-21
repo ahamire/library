bc3fe567a2008cc9dd4d98914984c997aa127471
ahamire <120735162+ahamire@users.noreply.github.com>
2023-09-21 22:23:25 +0300

    Add files via upload

---
+++ library/script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+class Book{
+    constructor(title,author,pages){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+  }
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
